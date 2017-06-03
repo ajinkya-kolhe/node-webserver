864166530c1ba0cb5466adf3c855f4e614b6d51b
Ajinkya Kolhe <ajinkya.k15@gmail.com>
2017-06-03 23:55:17 +1000

    Initial commit

---
+++ server.js
@@ -0,0 +1,70 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+var app = express();
+//console.log(__dirname);
+
+//HBS Register Partials
+hbs.registerPartials(__dirname + '/views/partials');
+
+//Set the Rendering View Engine
+app.set('view engine', 'hbs');
+
+//Express middleware 1
+app.use((request, respnose, next) => {
+  var now = new Date().toString();
+  var log = `${now}: ${request.method} ${request.path}`;
+  console.log(log);
+  fs.appendFile('server.log', log + '\n', (err) => {
+    if (err) {
+      console.log(err);
+    }
+  });
+  next();
+});
+
+//Express middleware 2
+app.use((request, respnose, next) => {
+  respnose.render('maintenance.hbs');
+});
+
+//Express middleware 3
+//Static directory
+app.use(express.static(__dirname + '/public'));
+
+//HBS Register Helper
+hbs.registerHelper('getCurrentYear', () => {
+  return new Date().getFullYear();
+});
+
+hbs.registerHelper('screamIt', (text) => {
+  return text.toUpperCase();
+});
+
+app.get('/', (request, respnose) => {
+  // respnose.send('<h1>Hello Express!<h1/>');
+  //Render the respnose
+  respnose.render('home.hbs', {
+    pageTitle: 'Home Page',
+    welcomeMessage: 'Welcome to my website'
+  });
+});
+
+app.get('/about', (request, respnose) => {
+  respnose.render('about.hbs', {
+    pageTitle: 'About Page',
+  });
+});
+
+app.get('/bad', (request, respnose) => {
+  //Send response
+  respnose.send({
+    errorMessage: 'Unable Handling Request'
+  });
+});
+
+//Port on which page will be displayed/rendered
+app.listen(3000, () => {
+  console.log('Server is up on 3000 port.');
+});
